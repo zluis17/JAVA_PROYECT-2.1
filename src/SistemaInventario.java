7e799f270b5543f46436dc2c51042acf3a24296e
Alejandrxx.18 <lalejandroquintero@ucompensar.edu.co>
2025-11-17 22:30:24 -0500

    Cuarto Commit: Clase venta, aumentar y reducir stock

---
+++ src/SistemaInventario.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList; 
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+
+class Producto{
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String  nombre, int cantidad, double precio){
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.precio = precio;
+}
+// Getters 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+// Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+public void reducirStock(int cantidadVendida){
+    this.cantidad -=cantidadVendida; 
+}
+
+public void aumentarStock(int cantidadVendida){
+    this.cantidad +=cantidadVendida;
+}
+
+public String toString(){
+    return "Producto: " + nombre + 
+            "| Cantidad: " + cantidad +
+            "| Precio: " +precio;
+}
+
+}
+
+class Venta{
+    private String nombreProducto;
+    private int cantidad;
+    private double precioUnitario;
+    private double total;
+    private String fecha;
+
+     // Constructor de Venta - Calcula automáticamente el total
+    public Venta(String nombreProducto, int cantidad, double precioUnitario) {
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.total = cantidad * precioUnitario;
+
+// Registrar fecha y hora de la venta
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        this.fecha = LocalDateTime.now().format(formatter);
+    }
+//Getters
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String toString(){
+       return "Fecha: " + fecha + "\n" +
+               "Producto: " + nombreProducto + "\n" +
+               "Cantidad: " + cantidad + " | Precio Unit: $" + String.format("%.2f", precioUnitario) + 
+               " | Total: $" + String.format("%.2f", total);
+    }
+    
+}
+
+
+//Clase principal - Sistema de Gestión de inventario y Ventas
+
+public class SistemaInventario {
+    private ArrayList<Producto> inventario;
+    private ArrayList<Venta> ventas;
+    private Scanner scanner;
+
+    public SistemaInventario(){
+        this.inventario = new ArrayList<>();
+        this.ventas = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java Test $$$$$");
+    }
+}
+
+
