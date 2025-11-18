9dc443b87429b949569278092cfe0eb0a4d461f9
Alejandrxx.18 <lalejandroquintero@ucompensar.edu.co>
2025-11-17 22:00:43 -0500

    Tercer Commit: Getters y Setters

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,33 @@ class Producto{
     this.cantidad = cantidad;
     this.precio = precio;
 }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+
 }
 
 
