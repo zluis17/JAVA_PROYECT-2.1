0b7a25cec6dd97c00d59cc6061216b37c2f352fc
Alejandrxx.18 <lalejandroquintero@ucompensar.edu.co>
2025-11-17 21:57:18 -0500

    Segundo Commit: Estructura y clases

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList; 
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+
+class Producto{
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String  nombre, int cantidad, double precio){
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.precio = precio;
+}
+}
+
+
+
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java Test $$$$$");
+    }
+}
